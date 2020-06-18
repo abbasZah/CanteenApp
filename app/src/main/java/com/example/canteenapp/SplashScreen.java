773946156a34361aa2c9f59b23e0cb323ad127e1
Abbas Zaheer <abbaszah123@gmail.com>
2020-06-18 19:25:44 +0500

    Added: UIs, SplashScreen, Logo

---
+++ app/src/main/java/com/example/canteenapp/SplashScreen.java
@@ -0,0 +1,41 @@
+package com.example.canteenapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+
+        getSupportActionBar().hide();
+
+        Thread thread = new Thread(){
+            public void run(){
+
+                try{
+                    sleep(2000);
+
+                }
+                catch (Exception e){
+                    e.printStackTrace();
+                }
+                finally {
+
+                    Intent i = new Intent(SplashScreen.this, Login.class);
+                    startActivity(i);
+
+                }
+
+            }
+        };
+        thread.start();
+
+
+    }
+}
